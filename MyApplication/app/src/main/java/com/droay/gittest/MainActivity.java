2e33e7316423fd2b46bbd40e752c4f5b5b36e858
droay <275298736@qq.com>
2018-08-29 23:12:30 +0800

    Update MainActivity.java
    加了个222

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"我是主干",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"我是主干222",Toast.LENGTH_LONG).show();
     }
 }
