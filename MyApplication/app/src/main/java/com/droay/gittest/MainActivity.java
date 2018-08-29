e6092ee9df90af0c9d989abecb69279e20c5200f
droay <275298736@qq.com>
2018-08-29 23:40:52 +0800

    init (cherry picked from commit 7ac7911)

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"我是主干2232",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"我是主干222",Toast.LENGTH_LONG).show();
     }
 }
