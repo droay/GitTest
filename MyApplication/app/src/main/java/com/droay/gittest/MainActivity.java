7ac7911309e1daf4a2bb87e43d47632c408adc5e
droay <275298736@qq.com>
2018-08-29 23:40:52 +0800

    init

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"我是主干222",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"我是主干333",Toast.LENGTH_LONG).show();
     }
 }
