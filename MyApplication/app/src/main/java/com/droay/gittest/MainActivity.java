9a75741ffeebae350f5b4fd708f70d945a914d89
wanga <275298736@qq.com>
2018-09-05 12:51:52 +0800

    升级gradle 升级SdkVersion

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -1,14 +1,16 @@
 package com.droay.gittest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"这是主分支，禁止push");
     }
 }
