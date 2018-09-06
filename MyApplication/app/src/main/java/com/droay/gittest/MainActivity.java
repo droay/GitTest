ffa8b9815c28aac975eeac926ec1b7f75ddd4a28
wanga <wanga@firmail.com>
2018-09-06 22:33:09 +0800

    rsp1  master2

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -1,12 +1,11 @@
 package com.droay.gittest;
 
 import android.os.Bundle;
-<<<<<<<<< Temporary merge branch 1
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-=========
 import android.widget.Toast;
->>>>>>>>> Temporary merge branch 2
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG="MainActivity";
