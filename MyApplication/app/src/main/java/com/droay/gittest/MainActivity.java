d4ae07f2ba4a31bf518d5355e81b263f621e9ca5
wanga <wanga@firmail.com>
2018-09-07 00:55:16 +0800

    rsp1  master rebase2

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"我是开发分支877？？？？？",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"我是开发分支888？？？？？",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"我是开发分支87722！！！！！！！",Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"teset rebase1",Toast.LENGTH_LONG).show();
-        Toast.makeText(this,"teset rebase2",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"teset rebase1？",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"teset rebase2！",Toast.LENGTH_LONG).show();
     }
 }
