a0f994b8fce63436d4b4ed4a0bb3d3c99957ee47
wanga <wanga@firmail.com>
2018-09-07 09:32:26 +0800

    rsp1  branch merge1

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -38,5 +38,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"branch apple1",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"branch apple2",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"branch apple3",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"branch merge1",Toast.LENGTH_LONG).show();
     }
 }
