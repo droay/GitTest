a0c079e638e037299901bde03fcdf759fb1aa500
wanga <wanga@firmail.com>
2018-09-07 09:26:41 +0800

    rsp1  master apple1

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -33,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"branch test1",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"branch test2",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"branch test3",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"teset apple1",Toast.LENGTH_LONG).show();
     }
 }
