34fcf89d133e4a05c71998bfb3cbc207591673c4
wanga <wanga@firmail.com>
2018-09-07 09:11:32 +0800

    rsp1  master rebase7

--- MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
+++ MyApplication/app/src/main/java/com/droay/gittest/MainActivity.java
@@ -27,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"branch2",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"teset rebase3",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"teset rebase4",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"teset rebase5",Toast.LENGTH_LONG).show();
     }
 }
